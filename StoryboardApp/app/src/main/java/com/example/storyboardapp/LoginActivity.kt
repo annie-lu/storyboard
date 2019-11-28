f83e46df7759782022bc2bca6e9d0c176e6142f9
Annie Lu <annie-lu@users.noreply.github.com>
2019-11-28 00:38:31 -0500

    Recreating the storyboard project and adding in a dummy layout

---
+++ StoryboardApp/app/src/main/java/com/example/storyboardapp/LoginActivity.kt
@@ -0,0 +1,101 @@
+package com.example.storyboardapp
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class LoginActivity : AppCompatActivity() {
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var userEmail: EditText? = null
+    private var userPassword: EditText? = null
+    private var loginBtn: Button? = null
+    private var progressBar: ProgressBar? = null
+
+    private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        initializeUI()
+
+        loginBtn!!.setOnClickListener { loginUserAccount() }
+    }
+
+    private fun loginUserAccount() {
+        progressBar!!.visibility = View.VISIBLE
+
+        // Todo : Retrieve eamil and password, make sure it's not empty
+
+        // Todo : Sigin with given Email and Password
+        // Retrieve UID for Current User if Login successful and store in intent, for the key UserID
+        // Start Intent DashboardActivity if Registration Successful
+
+        val email: String = userEmail!!.text.toString()
+        val password: String = userPassword!!.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(applicationContext, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(applicationContext, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+        Log.e(UserID, mAuth!!.getCurrentUser()!!.getUid())
+        mAuth!!.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    progressBar!!.visibility = View.GONE
+                    if (task.isSuccessful) {
+                        Toast.makeText(applicationContext, "Login successful!", Toast.LENGTH_LONG)
+                                .show()
+                        val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+                        Log.e(UserID, mAuth!!.uid)
+                        intent.putExtra(UserID,mAuth!!.uid)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                                applicationContext,
+                                "Login failed! Please try again later",
+                                Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+    }
+
+    private fun initializeUI() {
+        userEmail = findViewById(R.id.email)
+        userPassword = findViewById(R.id.password)
+
+        loginBtn = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+    }
+
+    companion object {
+        val UserMail = "com.example.tesla.myhomelibrary.UMail"
+        val UserID = "com.example.tesla.myhomelibrary.UID"
+
+    }
+}
--- StoryboardApp/app/src/main/java/com/example/storyboardapp/MainActivity.kt
+++ StoryboardApp/app/src/main/java/com/example/storyboardapp/MainActivity.kt
@@ -5,8 +5,27 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    internal var registerBtn: Button? = null
+    internal var loginBtn: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeViews()
+
+        registerBtn!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+        loginBtn!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun initializeViews() {
+        registerBtn = findViewById(R.id.register)
+        loginBtn = findViewById(R.id.login)
     }
 }
+
