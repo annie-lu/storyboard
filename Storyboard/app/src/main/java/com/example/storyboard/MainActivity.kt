2c575abb8d3610f637d97f216e9c01df77d0cf87
Anna Carandang <acara@terpmail.umd.edu>
2019-11-30 21:16:11 -0500

    Registration/Login works
    Changed google-services
    
    Changed firebase authentication rules to allow email auth
    
    Added error message for when registration fails
    
    TODO: Create new database entry when someone registers w/ empty works, userid, etc

--- Storyboard/app/src/main/java/com/example/storyboard/MainActivity.kt
+++ Storyboard/app/src/main/java/com/example/storyboard/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         loginBtn!!.setOnClickListener {
-            val intent = Intent(this@MainActivity, ProfileActivity::class.java)
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
         }
     }
--- Storyboard/app/src/main/java/com/example/storyboard/RegistrationActivity.kt
+++ Storyboard/app/src/main/java/com/example/storyboard/RegistrationActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -14,6 +15,9 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+
+
 
 class RegistrationActivity : AppCompatActivity() {
 
@@ -64,6 +68,10 @@ class RegistrationActivity : AppCompatActivity() {
                 } else {
                     Toast.makeText(applicationContext, "Registration failed! Please try again later", Toast.LENGTH_LONG).show()
                     progressBar!!.visibility = View.GONE
+                    val e = task.exception as FirebaseAuthException
+
+                    Log.i("Error, registration failed:", e.message)
+
                 }
             }
     }
