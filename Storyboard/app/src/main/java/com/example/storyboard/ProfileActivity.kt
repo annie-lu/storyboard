02bf5b83eb27f63e1e31dbde52c0a39c1440bed5
Anna Carandang <acara@terpmail.umd.edu>
2019-12-02 04:18:28 -0500

    Added edit button to profile
    Made it so you could only edit name+bio when you hit the edit button, and then disabled editing when you hit the edit button again

--- Storyboard/app/src/main/java/com/example/storyboard/ProfileActivity.kt
+++ Storyboard/app/src/main/java/com/example/storyboard/ProfileActivity.kt
@@ -1,22 +1,28 @@
 package com.example.storyboard
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.media.Image
 import android.os.Bundle
 import android.os.PersistableBundle
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.ListView
-import android.widget.TextView
+import android.text.InputType
+import android.util.Log
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 
+
 class ProfileActivity : AppCompatActivity() {
 
-    private var nameTV: TextView? = null
-    private var bioTV : TextView? = null
+    private var nameTV: EditText? = null
+    private var bioTV : EditText? = null
+    private var editButton : ImageButton? = null
     private var worksView: ListView? = null
 
     private var titles: MutableList<String>? = null
     private var mDatabase: DatabaseReference? = null
+
+    private var editing = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,11 +54,52 @@ class ProfileActivity : AppCompatActivity() {
         titles?.add("uwu211")
         titles?.add("hello211")
         titles?.add("whats up211")
+
+
+        //nameTV?.inputType = InputType.TYPE_NULL
+        //nameTV?.isEnabled = false
+        //nameTV?.isFocusable = false
+        //nameTV?.isClickable = true
+
+
+        editButton?.setOnClickListener {
+            editing = !editing
+
+            if (editing) {
+                (it as ImageButton).setColorFilter(Color.parseColor("#00BCD4"))
+
+            }
+            else {
+                (it as ImageButton).setColorFilter(android.R.color.transparent)
+            }
+
+            Toast.makeText(this, "Click button detected", Toast.LENGTH_SHORT).show()
+            nameTV?.isEnabled = !(nameTV!!.isEnabled)
+            bioTV?.isEnabled = !(bioTV!!.isEnabled)
+        }
+
+
+        //NOT WORKING
+        nameTV?.setOnClickListener {
+            Toast.makeText(this, "Long click detected", Toast.LENGTH_SHORT).show()
+            nameTV?.isEnabled = true
+            Log.i("HELP", "name clicked")
+            true
+        }
+
+        bioTV?.setOnLongClickListener {
+            Toast.makeText(this, "Long click detected", Toast.LENGTH_SHORT).show()
+            bioTV?.isEnabled = true
+            Log.i("HELP", "bio clicked")
+
+            true
+        }
     }
 
     private fun initializeUI() {
-        nameTV = findViewById(R.id.name)
-        bioTV = findViewById(R.id.bio)
+        nameTV = findViewById<EditText>(R.id.name)
+        bioTV = findViewById<EditText>(R.id.bio)
+        editButton = findViewById<ImageButton>(R.id.editButton)
         worksView = findViewById(R.id.worksView)
 
         titles = ArrayList()
@@ -62,6 +109,9 @@ class ProfileActivity : AppCompatActivity() {
             R.id.workTitle, titles!!)
 
         worksView?.adapter = adapter
+
+        nameTV?.isEnabled = false
+        bioTV?.isEnabled = false
 
 
     }
