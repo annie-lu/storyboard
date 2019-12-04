8fe3fc9b94ae5f83fb69d219932c40cad1356e00
Annie Lu <annie-lu@users.noreply.github.com>
2019-12-03 23:37:13 -0500

    last minute changes

--- Storyboard/app/src/main/java/com/example/storyboard/SubmitChallengeActivity.kt
+++ Storyboard/app/src/main/java/com/example/storyboard/SubmitChallengeActivity.kt
@@ -1,5 +1,6 @@
 package com.example.storyboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,15 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.appcompat.app.AlertDialog
 
 
 class SubmitChallengeActivity : AppCompatActivity() {
 
     private var worksView: ListView? = null
-
+    private var promptView: TextView? = null
 
     private var titles: MutableList<String>? = null
-    private var challenges: MutableList<String>? = null
+    private var submissions: MutableList<String>? = null
 
     private var spinner: Spinner ?= null
 
@@ -38,6 +40,8 @@ class SubmitChallengeActivity : AppCompatActivity() {
         works = intent.getStringExtra("WORKS").split(", ")
 
         mDatabase = FirebaseDatabase.getInstance()
+
+        mDatabaseChallenges= mDatabase!!.reference!!.child("Challenges")
         mDatabaseTitles = mDatabase!!.reference!!.child("Titles")
         titles = ArrayList()
         mDatabaseTitles!!.addValueEventListener(object : ValueEventListener {
@@ -49,38 +53,35 @@ class SubmitChallengeActivity : AppCompatActivity() {
             }
         })
 
-        challenges = ArrayList()
-        challenges?.add("5k Words")
-        challenges?.add("Creative Prompt")
+        mDatabaseChallenges!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                promptView!!.setText("Submit your work to complete this challenge! We add the challenge description here! " +dataSnapshot.child((intent.getStringExtra("CHALLENGE_NAME"))).child("Prompt").value)
+            } override fun onCancelled(databaseError: DatabaseError) {
+            }
+        })
+
+
+        submissions = ArrayList()
+        submissions?.add("Testing")
+        submissions?.add("Donuts")
 
 
         initializeUI()
-        submitBtn!!.setOnClickListener { submitChallengeWork() }
+        submitBtn!!.setOnClickListener { submitDialog() }
 
 
     }
 
     private fun initializeUI() {
 
+        promptView = findViewById(R.id.bio)
 
         val adapter = ArrayAdapter(this,
             R.layout.worksview_item,
-            R.id.workTitle, challenges!!)
+            R.id.workTitle, submissions!!)
 
         worksView = findViewById(R.id.worksView)
         worksView?.adapter = adapter
-
-        val spinner = findViewById<View>(R.id.spinner) as Spinner
-        val spinnerAdapter = ArrayAdapter<String>(
-            this, android.R.layout.simple_spinner_item,
-            titles!!
-        ) //selected item will look like a spinner set from XML
-        spinnerAdapter.setDropDownViewResource(
-            android.R.layout
-                .simple_spinner_dropdown_item
-        )
-        spinner.adapter = spinnerAdapter
-
 
         submitBtn = findViewById(R.id.submitButton)
 
@@ -88,45 +89,51 @@ class SubmitChallengeActivity : AppCompatActivity() {
 
     }
 
-    private fun submitChallengeWork(){
-        progressBar!!.visibility = View.VISIBLE
+    private fun submitDialog(){
 
-        val selected: String
-        selected = spinner!!.selectedItem.toString()
+            val dialogBuilder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogView = inflater.inflate(R.layout.submit_dialog, null)
+            dialogBuilder.setView(dialogView)
+
+            val spinner = dialogView.findViewById<View>(R.id.spinner) as Spinner
+            val spinnerAdapter = ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                titles!!
+            ) //selected item will look like a spinner set from XML
+            spinnerAdapter.setDropDownViewResource(
+                android.R.layout
+                    .simple_spinner_dropdown_item
+            )
+            spinner.adapter = spinnerAdapter
+
+            val buttonUpdate = dialogView.findViewById<View>(R.id.buttonSubmit) as Button
+
+            dialogBuilder.setTitle("Select Work to Edit/Delete")
+            // Log.e("help",mDatabaseUsers!!.child(authorId).child("Name").toString())
+            val b = dialogBuilder.create()
+            b.show()
 
 
-            //getting a unique id using push().getKey() method
-            //it will create a unique id and we will use it as the Primary Key for our Author
+            buttonUpdate.setOnClickListener {
 
+                val selected: String
+                selected = spinner!!.selectedItem.toString()
+                Toast.makeText(applicationContext, "Submitting "+selected, Toast.LENGTH_LONG).show()
 
-            //Saving the Author
-            Log.e("ohno", "attempts to create a challenge")
-            val current_challenge = intent.getStringExtra(CHALLENGE_NAME)
-            if( current_challenge == null) {
-                Log.i("help","this is null")
-            }else{
-                Log.i("help",current_challenge)
+                val current_challenge = intent.getStringExtra("CHALLENGE_NAME")
+
+                val uid = intent.getStringExtra(UserID)
+                mDatabaseChallenges!!.child(current_challenge).child(selected).setValue(works!![titles!!.indexOf(selected)])
+
+                b.dismiss()
+
             }
-            val uid = intent.getStringExtra(UserID)
-            mDatabaseChallenges!!.child(current_challenge).child(uid).setValue(selected)
 
-
-            //displaying a success toast
-
-            progressBar!!.visibility = View.INVISIBLE
-            Toast.makeText(this, "Challenge added", Toast.LENGTH_LONG).show()
-
-
-
-
-
-        }
-
+    }
 
 
 companion object {
-    val CHALLENGE_NAME = "com.example.tesla.myhomelibrary.authorname"
-    val AUTHOR_ID = "com.example.tesla.myhomelibrary.authorid"
     val UserID = "com.example.tesla.myhomelibrary.UID"
 }
 }
